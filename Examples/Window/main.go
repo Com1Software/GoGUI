cfa9e2155537f900da4d38c97b1a10e9f7b40128
Lenovo 15 <information@com1software.com>
2025-02-24 12:35:04 -0500

    upadte

--- Examples/Window/main.go
+++ Examples/Window/main.go
@@ -12,8 +12,17 @@ import (
 )
 
 func main() {
-	title := C.CString("My First Window")
+	title := C.CString("My First Test ")
 	defer C.free(unsafe.Pointer(title))
-	selectedItem := C.CreateMyWindow(title)
+
+	// Define menu items
+	menuItems := []string{"Option1", "Option2", "Option3", "Exit"}
+	cMenuItems := make([]*C.char, len(menuItems))
+	for i, item := range menuItems {
+		cMenuItems[i] = C.CString(item)
+		defer C.free(unsafe.Pointer(cMenuItems[i]))
+	}
+
+	selectedItem := C.CreateMyWindow(title, C.int(len(menuItems)), (**C.char)(unsafe.Pointer(&cMenuItems[0])))
 	fmt.Printf("Selected Menu Item: %s\n", C.GoString(selectedItem))
 }
