1b29507612e82a076c69ea1066ba185d9a104c5b
Lenovo 15 <information@com1software.com>
2025-02-25 13:47:57 -0500

    added

---
+++ Examples/InputMemoMsgBox/main.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../lib/inputmemomsgbox.c"
+*/
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	title := C.CString("Input Message Box")
+	defer C.free(unsafe.Pointer(title))
+
+	// Call the C function to create the input window and get the user input
+	result := C.CreateInputWindow(title)
+	fmt.Printf("User Input: %s\n", C.GoString(result))
+}
