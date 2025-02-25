0c84aed0a1b0015ba179f0dedd9769364a2187a2
Com1 Software, Inc. <information@com1software.com>
2025-02-25 09:59:18 -0500

    Update main.go

--- Examples/HelloWorld/main.go
+++ Examples/HelloWorld/main.go
@@ -19,37 +19,3 @@ func main() {
         panic(err)
     }
 }
-
-package main
-
-/*
-#ifdef _WIN32
-#include <windows.h>
-void MsgBox(const char* name, const char* title) {
-    MessageBox(NULL, name, title, MB_OK);
-}
-#else
-#include <stdio.h>
-#include <stdlib.h>
-void MsgBox(const char* name, const char* title) {
-    printf("%s: %s\n", title, name);
-}
-#endif
-*/
-import "C"
-import (
-    "unsafe"
-    "fmt"
-)
-
-func main() {
-    name := C.CString("Hello, World!")
-    title := C.CString("Greeting")
-    defer C.free(unsafe.Pointer(name))
-    defer C.free(unsafe.Pointer(title))
-
-    fmt.Println("Calling MsgBox function...")
-    // Call the C function to display the message box
-    C.MsgBox(name, title)
-    fmt.Println("MsgBox function called.")
-}
