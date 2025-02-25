92d06c344a68fc252079b3ecd5d124fb4d306963
Com1 Software, Inc. <information@com1software.com>
2025-02-25 09:57:31 -0500

    Update main.go

--- Examples/HelloWorld/main.go
+++ Examples/HelloWorld/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+    "github.com/andlabs/ui"
+)
+
+func main() {
+    err := ui.Main(func() {
+        window := ui.NewWindow("Greeting", 200, 100, false)
+        msg := ui.NewLabel("Hello, World!")
+        window.SetChild(msg)
+        window.OnClosing(func(*ui.Window) bool {
+            ui.Quit()
+            return true
+        })
+        window.Show()
+    })
+    if err != nil {
+        panic(err)
+    }
+}
+
+package main
+
 /*
 #ifdef _WIN32
 #include <windows.h>
